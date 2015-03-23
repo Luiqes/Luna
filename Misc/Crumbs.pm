06ac54f305c92f3a4c5e3ce17b9f0c3c74d68cc9
Romtoc <objcube@mail.com>
2015-03-23 14:33:49 +0530

    Small Changes

--- Misc/Crumbs.pm
+++ Misc/Crumbs.pm
@@ -1,10 +1,9 @@
+package Crumbs;
+
 use strict;
 use warnings;
 
-package Crumbs;
-
 use Method::Signatures;
-use Scalar::Util qw(reftype);
 use JSON qw(decode_json);
 use Cwd;
 
@@ -33,13 +32,13 @@ method new($resChild) {
                jitsu_cards => 'loadJitsuCards',
                redeem => 'loadRedemptions'
        };
-       $obj->{directory} = 'file://' . cwd() . '/Misc/JSON/';
+       $obj->{directory} = 'file://' . cwd . '/Misc/JSON/';
        return $obj;
 }
 
 method updateCrumbs {
        my $strDir = 'Misc/JSON/';
-       my @arrUrls = ();
+       my @arrUrls;
        while (my ($strKey, $strFile) = each(%{$self->{jsons}})) {
               if ($strKey ne 'pcards' && $strKey ne 'jcards' && $strKey ne 'redeem') {
                   my $strLink = 'http://media1.clubpenguin.com/play/en/web_service/game_configs/';
@@ -47,17 +46,28 @@ method updateCrumbs {
                   push(@arrUrls, $strUrl);
               }
        }
-       $self->{child}->{modules}->{tools}->asyncDownload($strDir, \@arrUrls);
+       eval {
+          $self->{child}->{modules}->{tools}->asyncDownload($strDir, \@arrUrls);
+       };
+       if ($@) {
+           return $self->{child}->{modules}->{logger}->output('Failed To Update Crumbs Due To Error: ' . $@, Logger::LEVELS->{err});
+       }
        $self->{child}->{modules}->{logger}->output('Successfully Updated Crumbs', Logger::LEVELS->{inf});
 }
 
 method loadCrumbs {
-       my @arrFiles = ();
-       foreach my $strUrl (values %{$self->{jsons}}) {
-          my $strFile = $self->{directory} . $strUrl;
-          push(@arrFiles, $strFile);
+       my @arrFiles;
+       foreach (values %{$self->{jsons}}) {
+                my $strFile = $self->{directory} . $_;
+                push(@arrFiles, $strFile);
        }
-       my $arrInfo = $self->{child}->{modules}->{tools}->asyncGetContent(\@arrFiles);
+       my $arrInfo;
+       eval {
+          $arrInfo = $self->{child}->{modules}->{tools}->asyncGetContent(\@arrFiles);
+       };
+       if ($@) {
+           return $self->{child}->{modules}->{logger}->output('Failed To Load Crumbs Due To Error: ' . $@, Logger::LEVELS->{err});
+       }
        while (my ($strKey, $arrData) = each(%{$arrInfo})) {
               if (exists($self->{methods}->{$strKey})) {
                   my $strMethod = $self->{methods}->{$strKey};
@@ -69,16 +79,21 @@ method loadCrumbs {
 }
 
 method loadItems($arrItems) {
-       foreach my $strItem (sort @{$arrItems}) {
-          %{$self->{itemCrumbs}->{$strItem->{paper_item_id}}} = (cost => $strItem->{cost}, type => $strItem->{type}, isBait => $strItem->{is_bait});
+       foreach (sort @{$arrItems}) {
+                if ($_->{is_epf}) {
+                    %{$self->{epfCrumbs}->{$_->{paper_item_id}}} = (points => $_->{cost});               
+                } else {
+                    %{$self->{itemCrumbs}->{$_->{paper_item_id}}} = (cost => $_->{cost}, type => $_->{type}, isBait => $_->{is_bait});
+                }
        }
        $self->{child}->{modules}->{logger}->output('Successfully Loaded ' . scalar(keys %{$self->{itemCrumbs}}) . ' Items', Logger::LEVELS->{inf});
+       $self->{child}->{modules}->{logger}->output('Successfully Loaded ' . scalar(keys %{$self->{epfCrumbs}}) . ' EPF Items', Logger::LEVELS->{inf});
 }
 
 method loadStamps($arrStamps) {
        foreach my $arrIndexStamps (sort @{$arrStamps}) {
           foreach my $arrIndexTwoStamps (sort %{$arrIndexStamps}) {
-             if (reftype($arrIndexTwoStamps)) {
+             if (ref($arrIndexTwoStamps) eq 'ARRAY') {
                  foreach my $strStamp (sort @{$arrIndexTwoStamps}) {
                     %{$self->{stampCrumbs}->{$strStamp->{stamp_id}}} = (rank => $strStamp->{rank});
 				            }	
@@ -89,35 +104,39 @@ method loadStamps($arrStamps) {
 }
 
 method loadIgloos($arrIgloos) {
-       foreach my $strIgloo (sort keys %{$arrIgloos}) {
-          %{$self->{iglooCrumbs}->{$arrIgloos->{$strIgloo}->{igloo_id}}} = (cost => $arrIgloos->{$strIgloo}->{cost});    
+       foreach (sort keys %{$arrIgloos}) {
+                %{$self->{iglooCrumbs}->{$arrIgloos->{$_}->{igloo_id}}} = (cost => $arrIgloos->{$_}->{cost});    
        }
        $self->{child}->{modules}->{logger}->output('Successfully Loaded ' . scalar(keys %{$self->{iglooCrumbs}}) . ' Igloos', Logger::LEVELS->{inf});
 }
 
 method loadFloors($arrFloors) {
-       foreach my $strFloor (sort @{$arrFloors}) {
-          %{$self->{floorCrumbs}->{$strFloor->{igloo_floor_id}}} = (cost => $strFloor->{cost});
+       foreach (sort @{$arrFloors}) {
+                %{$self->{floorCrumbs}->{$_->{igloo_floor_id}}} = (cost => $_->{cost});
        }
        $self->{child}->{modules}->{logger}->output('Successfully Loaded ' . scalar(keys %{$self->{floorCrumbs}}) . ' Floors', Logger::LEVELS->{inf});
 }
 
 method loadFurnitures($arrFurns) {
-       foreach my $strFurn (sort @{$arrFurns}) {
-          %{$self->{furnitureCrumbs}->{$strFurn->{furniture_item_id}}} = (cost => $strFurn->{cost});
+       foreach (sort @{$arrFurns}) {
+                %{$self->{furnitureCrumbs}->{$_->{furniture_item_id}}} = (cost => $_->{cost});
        }
        $self->{child}->{modules}->{logger}->output('Successfully Loaded ' . scalar(keys %{$self->{furnitureCrumbs}}) . ' Furnitures', Logger::LEVELS->{inf});
 }
 
 method loadRooms($arrRooms) {
-       foreach my $strRoom (sort keys %{$arrRooms}) {
-          my $intRoom = $arrRooms->{$strRoom}->{room_id};
-          my $intLimit = $arrRooms->{$strRoom}->{max_users};
-          if ($intRoom <= 899) {
+       foreach (sort keys %{$arrRooms}) {
+          my $intRoom = $arrRooms->{$_}->{room_id};
+          my $intLimit = $arrRooms->{$_}->{max_users};
+          my $strKey = $arrRooms->{$_}->{room_key};
+          if ($strKey ne '') {
 	             %{$self->{roomCrumbs}->{$intRoom}} = (limit => $intLimit);
+          } else {
+              %{$self->{gameRoomCrumbs}->{$intRoom}} = (limit => $intLimit);
           }
        }
        $self->{child}->{modules}->{logger}->output('Successfully Loaded ' . scalar(keys %{$self->{roomCrumbs}}) . ' Rooms', Logger::LEVELS->{inf});
+       $self->{child}->{modules}->{logger}->output('Successfully Loaded ' . scalar(keys %{$self->{gameRoomCrumbs}}) . ' Game Rooms', Logger::LEVELS->{inf});
 }
 
 method loadPostcards($arrPostcards) {
@@ -127,16 +146,9 @@ method loadPostcards($arrPostcards) {
        $self->{child}->{modules}->{logger}->output('Successfully Loaded ' . scalar(keys %{$self->{mailCrumbs}}) . ' Postcards', Logger::LEVELS->{inf});
 }
 
-method loadJitsuCards($arrCards) {
-       foreach my $strCard (sort keys %{$arrCards}) {
-          %{$self->{jitsuCrumbs}->{$arrCards->{$strCard}->{id}}} = (power => $arrCards->{$strCard}->{power}, destruction => $arrCards->{$strCard}->{damage}, attribute => $arrCards->{$strCard}->{attribute}, colour => $arrCards->{$strCard}->{color});                                                    
-       }
-       $self->{child}->{modules}->{logger}->output('Successfully Loaded ' . scalar(keys %{$self->{jitsuCrumbs}}) . ' Jitsu Cards', Logger::LEVELS->{inf});
-}
-
 method loadRedemptions($arrRedemptions) {
-       foreach my $strRedemption (sort keys %{$arrRedemptions}) {
-          %{$self->{redeemCrumbs}->{$arrRedemptions->{$strRedemption}->{redeemName}}} = (type => $arrRedemptions->{$strRedemption}->{redeemType}, items => $arrRedemptions->{$strRedemption}->{redeemItems}, cost => $arrRedemptions->{$strRedemption}->{redeemCoins});
+       foreach (sort keys %{$arrRedemptions}) {
+                %{$self->{redeemCrumbs}->{$arrRedemptions->{$_}->{redeemName}}} = (type => $arrRedemptions->{$_}->{redeemType}, items => $arrRedemptions->{$_}->{redeemItems}, cost => $arrRedemptions->{$_}->{redeemCoins});
        }
        $self->{child}->{modules}->{logger}->output('Successfully Loaded ' . scalar(keys %{$self->{redeemCrumbs}}) . ' Redemptions', Logger::LEVELS->{inf}); 
 }
