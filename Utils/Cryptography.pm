58f35e291ce26cf22ebff23bef4dfc8d5b28fef5
Romtoc <objcube@mail.com>
2015-03-23 14:35:10 +0530

    Renamed To Cryptography

---
+++ Utils/Cryptography.pm
@@ -0,0 +1,46 @@
+package Cryptography;
+
+use strict;
+use warnings;
+
+use Method::Signatures;
+use Digest::MD5 qw(md5_hex);
+
+method new {
+       my $obj = bless {}, $self;
+       return $obj;
+}
+
+method encryptPass(Str $strPassword, Str $strKey) {
+       my $strHash = $self->swapMD5(md5_hex($self->swapMD5($strPassword) . $strKey . 'Y(02.>\'H}t":E1'));
+       return $strHash;
+}
+
+method swapMD5(Str $strHash) {
+       my $strSwapped = substr($strHash, 16, 16);
+       $strSwapped .= substr($strHash, 0, 16);
+       return $strSwapped;
+}
+
+method reverseMD5(Str $strKey) {
+       my $revKey = reverse($strKey);
+       my $strHash = md5_hex($revKey);
+       return $strHash;
+}
+
+method generateKey {
+       my @arrChars = ('A'..'Z', 'a'..'z', 0..9, '!\"\A3$%^&*()_+-=[]{}:@~;#<>?|\\,./');
+       my $strKey = '';
+       for (0..9) {
+            $strKey .= $arrChars[rand(@arrChars)];
+       }
+       return $strKey;
+}
+
+method generateInt(Int $intMin, Int $intMax) {
+       my $intRand = rand($intMax - $intMin);
+       my $intFinal = int($intMin + $intRand);
+       return $intFinal;
+}
+
+1;
